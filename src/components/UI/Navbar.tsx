ca5aa872f7fbe2a0ea31143d406b2c4bccd123fb
dbarattini-pg <dbarattini-pg@outlook.it>
2023-05-01 15:46:32 +0200

    feat: add router example

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
+
+  function goBack() {
+    router.back();
+  }
+
   return (
     <nav className={styles.container}>
+      {router.pathname !== "/" && (
+        <button className={styles.backButton} onClick={goBack}>
+          back
+        </button>
+      )}
       <Link href="/" className={styles.link}>
         <h1 className={styles.title}>Navbar</h1>
       </Link>
