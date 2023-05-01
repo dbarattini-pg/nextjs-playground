f03734d9e26ebbc5f1a66adc1380c94260b6f9da
dbarattini-pg <dbarattini-pg@outlook.it>
2023-05-01 15:46:23 +0200

    feat: add style examples

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
+import styles from "./Post.module.css";
+
 type PostProps = {
   post: Post;
 };
 
 export default function Post({ post }: PostProps) {
   return (
-    <div>
-      <h1>{post.title}</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{post.title}</h1>
       <p>{post.body}</p>
     </div>
   );
