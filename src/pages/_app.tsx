d175e51472bfca8512312347f8a80179ddd9c86c
dbarattini-pg <dbarattini-pg@outlook.it>
2023-05-01 15:46:25 +0200

    feat: add font optimization example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import Navbar from "@/components/UI/Navbar";
 import "@/styles/globals.css";
+import { Roboto_Flex } from "next/font/google";
 import type { AppProps } from "next/app";
+
+// If loading a variable font, you don't need to specify the font weight
+const robotoFlex = Roboto_Flex({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={robotoFlex.className}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
