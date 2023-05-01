7512d94f58144ef52f7212ed568c5787bc89aee4
dbarattini-pg <dbarattini-pg@outlook.it>
2023-05-01 15:46:24 +0200

    feat: add global layout example

---
+++ src/components/UI/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.container}>
+      <h1 className={styles.title}>Navbar</h1>
+    </nav>
+  );
+}
