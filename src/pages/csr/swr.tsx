0c07a8bdc54fa6e621fe7b4645c7fc4724cf8a25
dbarattini-pg <dbarattini-pg@outlook.it>
2023-05-01 15:46:21 +0200

    feat: add csr swr example

---
+++ src/pages/csr/swr.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import useSWR, { Fetcher } from "swr";
+
+export default function CsrSwr() {
+  const fetcher: Fetcher<Post[], string> = (url) =>
+    fetch(url).then((res) => res.json());
+
+  const {
+    data: posts,
+    error,
+    isLoading,
+  } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher);
+
+  console.log(isLoading);
+
+  if (error) return <p>An error has occurred.</p>;
+  if (isLoading) return <p>Loading...</p>;
+  if (!posts) return <p>No Posts</p>;
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
